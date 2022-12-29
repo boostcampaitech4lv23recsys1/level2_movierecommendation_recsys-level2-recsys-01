84aca48da9a9ec05b85538c8fe8b5cdc68b71c81
Jeong-Junhwan <zxxzx1515@naver.com>
2022-12-29 14:38:48 +0000

    [Chore] train.py 대략적 그림
    - 조금씩 돌려보면서 디버깅 하기 위해 train.py 대략적으로 작성
    - 기존 k-fold, sweep 방식 제거 (시간이 너무 오래걸림)
    - preprocess에서 오류 발생하는 것 확인
    - dataset, dataloader 사이에서 valid관련 통일 필요

--- data_loader/preprocess.py
+++ data_loader/preprocess.py
@@ -8,44 +8,76 @@ import os
 class Preprocess:
     def __init__(self, config):
         self.config = config
-        self.data_path = config["data_path"]
-        
-        self.org_director_data = pd.read_csv(os.path.join(self.data_path, 'directors.tsv'), sep='\t')
-        self.org_year_data = pd.read_csv(os.path.join(self.data_path, 'years.tsv'), sep='\t')
-        self.org_writer_data = pd.read_csv(os.path.join(self.data_path, 'writers.tsv'), sep='\t')
-        self.org_title_data = pd.read_csv(os.path.join(self.data_path, 'titles.tsv'), sep='\t')
-        self.org_genre_data = pd.read_csv(os.path.join(self.data_path, 'genres.tsv'), sep='\t')
-        self.org_train_data = pd.read_csv(os.path.join(self.data_path, 'train_ratings.csv'))
+        self.data_path = config["preprocess"]["data_dir"]
 
-    def __grouping_data(self, data:pd.DataFrame, column_name:str) -> pd.DataFrame:
-        return data.groupby('item')[column_name].apply(','.join).reset_index(drop=True)
-    
+        self.org_director_data = pd.read_csv(
+            os.path.join(self.data_path, "directors.tsv"), sep="\t"
+        )
+        self.org_year_data = pd.read_csv(
+            os.path.join(self.data_path, "years.tsv"), sep="\t"
+        )
+        self.org_writer_data = pd.read_csv(
+            os.path.join(self.data_path, "writers.tsv"), sep="\t"
+        )
+        self.org_title_data = pd.read_csv(
+            os.path.join(self.data_path, "titles.tsv"), sep="\t"
+        )
+        self.org_genre_data = pd.read_csv(
+            os.path.join(self.data_path, "genres.tsv"), sep="\t"
+        )
+        self.org_train_data = pd.read_csv(
+            os.path.join(self.data_path, "train_ratings.csv")
+        )
+
+    def __grouping_data(self, data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+        return data.groupby("item")[column_name].apply(",".join).reset_index(drop=True)
+
     def __merge_data(self):
-        self.director_data = self.__grouping_data(self.org_director_data, 'director')
-        self.writer_data = self.__grouping_data(self.org_writer_data, 'writer')
-        self.genre_data = self.__grouping_data(self.org_genre_data, 'genre')
-        datas = [self.org_train_data, self.org_title_data, self.org_year_data,
-                 self.director_data, self.writer_data, self.genre_data]
-        self.data = reduce(lambda left, right : pd.merge(left, right, on='item', how='left'), datas)
+        self.director_data = self.__grouping_data(self.org_director_data, "director")
+        self.writer_data = self.__grouping_data(self.org_writer_data, "writer")
+        self.genre_data = self.__grouping_data(self.org_genre_data, "genre")
+        datas = [
+            self.org_train_data,
+            self.org_title_data,
+            self.org_year_data,
+            self.director_data,
+            self.writer_data,
+            self.genre_data,
+        ]
+        self.data = reduce(
+            lambda left, right: pd.merge(left, right, on="item", how="left"), datas
+        )
         return self.data
-        
+
     def __preprocessing_year_title(self):
-        self.data['year'] = self.data['title'].apply(lambda x : re.search('\(\d{4}', x).group()[1:])
-        self.data['title'] = self.data['title'].apply(lambda x : re.sub('\(\d+-{0,1}\d*\)','', x))
+        self.data["year"] = self.data["title"].apply(
+            lambda x: re.search("\(\d{4}", x).group()[1:]
+        )
+        self.data["title"] = self.data["title"].apply(
+            lambda x: re.sub("\(\d+-{0,1}\d*\)", "", x)
+        )
         return self.data
-        
+
     def __preprocessing_timestamp(self):
-        self.data['watch_year'] =self.data['time'].apply(lambda x : time.strftime('%Y', time.localtime(x)))
-        self.data['watch_month'] =self.data['time'].apply(lambda x : time.strftime('%m', time.localtime(x)))
-        self.data['watch_day'] =self.data['time'].apply(lambda x : time.strftime('%d', time.localtime(x)))
-        self.data['watch_hour'] =self.data['time'].apply(lambda x : time.strftime('%H', time.localtime(x)))
+        self.data["watch_year"] = self.data["time"].apply(
+            lambda x: time.strftime("%Y", time.localtime(x))
+        )
+        self.data["watch_month"] = self.data["time"].apply(
+            lambda x: time.strftime("%m", time.localtime(x))
+        )
+        self.data["watch_day"] = self.data["time"].apply(
+            lambda x: time.strftime("%d", time.localtime(x))
+        )
+        self.data["watch_hour"] = self.data["time"].apply(
+            lambda x: time.strftime("%H", time.localtime(x))
+        )
         return self.data
 
     def __preprocessing_NA(self):
-        self.data['director'] = self.data['director'].fillna('others')
-        self.data['writer'] = self.data['writer'].fillna('others')
+        self.data["director"] = self.data["director"].fillna("others")
+        self.data["writer"] = self.data["writer"].fillna("others")
         return self.data
-        
+
     def preprocessing(self):
         data = self.__merge_data()
         data = self.__preprocessing_year_title()
