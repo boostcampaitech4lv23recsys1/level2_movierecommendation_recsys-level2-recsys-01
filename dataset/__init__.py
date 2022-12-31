9cce2bb340125b86fb609a263b551cb9a3237d71
wonjun <jwj51720@gmail.com>
2022-12-31 06:17:18 +0000

    [Feat] SeqNegPreDataset 구현 - dataset.py 내에 class 구현 - SeqNegDataset 구현 예정

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,2 +1,3 @@
-from .datasets import SeqDataset, NonSeqDataset
+from .base_dataset import SeqDataset, NonSeqDataset
+from .dataset import SeqNegPreDataset
 from .get_dataset import get_dataset
--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,175 +1,146 @@
 import torch
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-import numpy as np
-from sklearn import preprocessing
 import random
+from dataset import SeqDataset, NonSeqDataset
+from utils import neg_sample
+from typing import Tuple
 
-import pandas as pd
-
-class BaseDataset(Dataset):
+class SeqNegPreDataset(SeqDataset):
     def __init__(
-            self,
-            data: Dataset,
-            idx: list,
-            config: dict,
+        self, 
+        user_seq: list, 
+        long_sequence: list,
+        **kwargs
     ) -> None:
-        super().__init__()
-        self.data = data[data["userID"].isin(idx)]
-        self.user_list = self.data["userID"].unique().tolist()
-        self.config = config
-        self.max_seq_len = config["dataset"]["max_seq_len"]
+        # user_seq, long_sequence 구한 것을 받는 형태로 작동
+        super().__init__(**kwargs)
+        # preprocess.py에서 val2idx 하고 넘겨줘야 함
+        self.cur_cat_col = [f"{col}2idx" for col in self.config["cat_cols"]] + ["user"]
+        self.cur_num_col = self.config["num_cols"] + ["user"]
 
-        self.Y = self.data.groupby("userID")["answerCode"]
-
-        self.cur_cat_col = [f"{col}2idx" for col in config["cat_cols"]] + ["userID"]
-        self.cur_num_col = config["num_cols"] + ["userID"]
         self.X_cat = self.data.loc[:, self.cur_cat_col].copy()
         self.X_num = self.data.loc[:, self.cur_num_col].copy()
 
-        self.X_cat = self.X_cat.groupby("userID")
-        self.X_num = self.X_num.groupby("userID")
+        self.X_cat = self.X_cat.groupby("user")
+        self.X_num = self.X_num.groupby("user")
 
-        self.group_data = self.data.groupby("userID")
+        self.group_data = self.data.groupby("user")
 
+        self.user_seq = user_seq # 유저별 본 영화 시퀀스
+        self.long_sequence = long_sequence # data상 모든 영화 나열
+        self.part_sequence = [] # 시퀀스를 계단식으로 쪼갤것임
+        self.split_sequence()
+
+    def split_sequence(self) -> None:
+        for seq in self.user_seq:
+            input_ids = seq[-(self.max_seq_len + 2) : -2]  # keeping same as train set
+            for i in range(len(input_ids)):
+                self.part_sequence.append(input_ids[: i + 1])
+    
     def __len__(self) -> int:
-        """
-        return data length
-        """
-        return len(self.user_list)
+        return len(self.part_sequence)
+        
+    def __getitem__(self, index) -> Tuple[torch.tensor, ...]:
+        sequence = self.part_sequence[index]  # pos_items
+        # sample neg item for every masked item
+        masked_item_sequence = []
+        neg_items = []
+        # Masked Item Prediction
+        item_set = set(sequence)
+        for item in sequence[:-1]:
+            prob = random.random() # 0~1 사이의 난수 생성
+            if prob < self.config_dataset["mask_p"]: # neg_items 선정
+                masked_item_sequence.append(self.config["mask_id"])
+                neg_items.append(neg_sample(item_set, self.config["item_size"]))
+            else: # 그 아이템을 neg_items에 넣음
+                masked_item_sequence.append(item)
+                neg_items.append(item)
 
-    def __getitem__(self, index: int) -> object:
-        user = self.user_list[index]
-        cat = self.X_cat.get_group(user).values[:, :-1]
-        num = self.X_num.get_group(user).values[:, :-1].astype(np.float32)
-        y = self.Y.get_group(user).values
-        seq_len = cat.shape[0]
+        # add mask at the last position
+        masked_item_sequence.append(self.config["mask_id"])
+        neg_items.append(neg_sample(item_set, self.config["item_size"]))
 
-        if seq_len >= self.max_seq_len:
-            cat = torch.tensor(cat[-self.max_seq_len :], dtype=torch.long)
-            num = torch.tensor(num[-self.max_seq_len :], dtype=torch.float32)
-            y = torch.tensor(y[-self.max_seq_len :], dtype=torch.float32)
-            mask = torch.ones(self.max_seq_len, dtype=torch.long)
+        # Segment Prediction
+        if len(sequence) < 2:
+            masked_segment_sequence = sequence
+            pos_segment = sequence
+            neg_segment = sequence
         else:
-            cat = torch.cat(
-                (
-                    torch.zeros(
-                        self.max_seq_len - seq_len,
-                        len(self.cur_cat_col) - 1,
-                        dtype=torch.long,
-                    ),
-                    torch.tensor(cat, dtype=torch.long),
-                )
+            sample_length = random.randint(1, len(sequence) // 2)
+            start_id = random.randint(0, len(sequence) - sample_length)
+            neg_start_id = random.randint(0, len(self.long_sequence) - sample_length)
+            pos_segment = sequence[start_id : start_id + sample_length]
+            neg_segment = self.long_sequence[
+                neg_start_id : neg_start_id + sample_length
+            ]
+            masked_segment_sequence = (
+                sequence[:start_id]
+                + [self.config["mask_id"]] * sample_length
+                + sequence[start_id + sample_length :]
             )
-            num = torch.cat(
-                (
-                    torch.zeros(
-                        self.max_seq_len - seq_len,
-                        len(self.cur_num_col) - 1,
-                        dtype=torch.float32,
-                    ),
-                    torch.tensor(num, dtype=torch.float32),
-                )
+            pos_segment = (
+                [self.config["mask_id"]] * start_id
+                + pos_segment
+                + [self.config["mask_id"]] * (len(sequence) - (start_id + sample_length))
             )
-            y = torch.cat(
-                (
-                    torch.zeros(self.max_seq_len - seq_len, dtype=torch.float32),
-                    torch.tensor(y, dtype=torch.float32),
-                )
+            neg_segment = (
+                [self.config["mask_id"]] * start_id
+                + neg_segment
+                + [self.config["mask_id"]] * (len(sequence) - (start_id + sample_length))
             )
-            mask = torch.zeros(self.max_seq_len, dtype=torch.long)
-            mask[-seq_len:] = 1
 
-        return {"cat": cat, "num": num, "answerCode": y, "mask": mask}
+        assert len(masked_segment_sequence) == len(sequence)
+        assert len(pos_segment) == len(sequence)
+        assert len(neg_segment) == len(sequence)
+
+        # padding sequence
+        pad_len = self.max_seq_len - len(sequence)
+        masked_item_sequence = [0] * pad_len + masked_item_sequence
+        pos_items = [0] * pad_len + sequence
+        neg_items = [0] * pad_len + neg_items
+        masked_segment_sequence = [0] * pad_len + masked_segment_sequence
+        pos_segment = [0] * pad_len + pos_segment
+        neg_segment = [0] * pad_len + neg_segment
+
+        masked_item_sequence = masked_item_sequence[-self.max_seq_len :]
+        pos_items = pos_items[-self.max_seq_len :]
+        neg_items = neg_items[-self.max_seq_len :]
+
+        masked_segment_sequence = masked_segment_sequence[-self.max_seq_len :]
+        pos_segment = pos_segment[-self.max_seq_len :]
+        neg_segment = neg_segment[-self.max_seq_len :]
+
+        # Associated Attribute Prediction
+        # Masked Attribute Prediction
+        attributes = []
+        for item in pos_items:
+            attribute = [0] * self.config["attribute_size"]
+            try:
+                now_attribute = self.config["item2attribute"][str(item)]
+                for a in now_attribute:
+                    attribute[a] = 1
+            except:
+                pass
+            attributes.append(attribute)
+
+        assert len(attributes) == self.max_seq_len
+        assert len(masked_item_sequence) == self.max_seq_len
+        assert len(pos_items) == self.max_seq_len
+        assert len(neg_items) == self.max_seq_len
+        assert len(masked_segment_sequence) == self.max_seq_len
+        assert len(pos_segment) == self.max_seq_len
+        assert len(neg_segment) == self.max_seq_len
+
+        cur_tensors = (
+            torch.tensor(attributes, dtype=torch.long),
+            torch.tensor(masked_item_sequence, dtype=torch.long),
+            torch.tensor(pos_items, dtype=torch.long),
+            torch.tensor(neg_items, dtype=torch.long),
+            torch.tensor(masked_segment_sequence, dtype=torch.long),
+            torch.tensor(pos_segment, dtype=torch.long),
+            torch.tensor(neg_segment, dtype=torch.long),
+        )
+        return cur_tensors
 
 
-def collate_fn(batch):
-    """
-    [batch, data_len, dict] -> [dict, batch, data_len]
-    """
-    X_cat, X_num, y, mask = [], [], [], []
-    for user in batch:
-        X_cat.append(user["cat"])
-        X_num.append(user["num"])
-        y.append(user["answerCode"])
-        mask.append(user["mask"])
-
-    return {
-        "cat": torch.stack(X_cat),
-        "num": torch.stack(X_num),
-        "answerCode": torch.stack(y),
-        "mask": torch.stack(mask),
-    }
-
-
-def get_loader(
-        train_set: Dataset,
-        val_set: Dataset,
-        config: dict
-) -> DataLoader:
-    """
-    get Data Loader
-    """
-    train_loader = DataLoader(
-        train_set,
-        num_workers=config["num_workers"],
-        shuffle=True,
-        batch_size=config["batch_size"],
-        collate_fn=collate_fn,
-    )
-    valid_loader = DataLoader(
-        val_set,
-        num_workers=config["num_workers"],
-        shuffle=False,
-        batch_size=config["batch_size"],
-        collate_fn=collate_fn,
-    )
-
-    return train_loader, valid_loader
-
-
-class XGBoostDataset(object):
-    def __init__(
-        self,
-        config: dict,
-    ) -> None:
-        self.config = config
-        self.train = self._load_train_data()
-        self.test = self._load_test_data()
-        self.user_ids = list(self.train["userID"].unique())
-        self.test_ids = list(self.test["userID"].unique())
-        self.__preprocessing()
-
-    def __preprocessing(self):
-        cat_cols = self.config.cat_cols
-
-        label_encoder = preprocessing.LabelEncoder()
-        for cat_col in cat_cols:
-            self.train[cat_col] = label_encoder.fit_transform(self.train[cat_col])
-
-        self.train = self.train.reset_index(drop=True)
-
-    def _split(
-        self,
-        ratio=0.9,
-    ):
-        train_df = self.train[~self.train["answerCode"] == -1]
-
-        random.shuffle(self.user_ids)
-
-        train_len = int(len(self.user_ids) * ratio)
-        train_ids = self.user_ids[:train_len]
-        test_ids = self.user_ids[train_len:]
-
-        train = train_df[train_df["userID"].isin(train_ids)]
-        valid = train_df[train_df["userID"].isin(test_ids)]
-
-        valid = valid[valid["userID"] != valid["userID"].shift(-1)]
-
-        return train, valid
-
-    def _load_train_data(self):
-        return pd.read_csv(self.config.data_path)
-
-    def _load_test_data(self):
-        return pd.read_csv(self.config.test_path)
+class SeqNegDataset(SeqDataset):
+    raise NotImplementedError
