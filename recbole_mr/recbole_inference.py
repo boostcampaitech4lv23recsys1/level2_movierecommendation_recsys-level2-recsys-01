3ab56884cfefde6738ce217be33f27550b94c26e
41ow1ives <77601251@daum.net>
2023-01-02 12:37:32 +0000

    [Feat]: Inference py and yaml
    - inference.yaml file can set save path and load data path with model
    name
    - Run recbole_inference.py creates inference csv file

---
+++ recbole_mr/recbole_inference.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd
+import numpy as np
+import torch
+from tqdm import tqdm
+from recbole.quick_start.quick_start import load_data_and_model
+from recbole.utils.case_study import full_sort_topk
+import yaml
+from yaml.loader import SafeLoader
+
+if __name__ == "__main__":
+    with open('./recbole_mr/yaml/inference.yaml') as f:
+        config = yaml.load(f, Loader=SafeLoader)
+
+    user = pd.read_csv(config['submission_file_path'])
+    user_index = np.array(user['user'].drop_duplicates(), dtype=str)
+
+    config, model, dataset, train_data, valid_data, test_data = load_data_and_model(
+        model_file=os.path.join(config['model_path'], config['model_name'])
+    )
+    del train_data, valid_data
+
+    uid_series = dataset.token2id(dataset.uid_field, user_index)
+
+    total_topk_score, total_topk_iid_list = torch.zeros_like(
+        torch.Tensor(31360, 10)
+    ), torch.zeros_like(torch.Tensor(31360, 10))
+    for idx, uid_idx in enumerate(tqdm(range(0, len(uid_series)))):
+        topk_score, topk_iid_list = full_sort_topk(
+            np.array([uid_series[uid_idx]]),
+            model,
+            test_data,
+            k=10,
+            device=config['device'],
+        )
+        total_topk_score[idx] = topk_score
+        total_topk_iid_list[idx] = topk_iid_list
+
+    int_iid = total_topk_iid_list.to(torch.int64)
+    external_item_list = dataset.id2token(dataset.iid_field, int_iid.cpu())
+    external_item_list = external_item_list.flatten()
+    df = pd.DataFrame({'user': user_index, 'item': external_item_list})
+    df.to_csv(
+        os.path.join(config['inference_file_path'], config['model_name'] + '.csv'),
+        index=False,
+    )
